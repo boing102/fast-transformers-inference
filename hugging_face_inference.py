d8b0d905d1e41117099d48c1a1f3f0c365c20593
boing102 <mmekota@zendesk.com>
2022-11-01 12:40:53 +0000

    Add endpoint creation

--- hugging_face_inference.py
+++ hugging_face_inference.py
@@ -1,11 +1,13 @@
 from sagemaker.huggingface import HuggingFaceModel
 import sagemaker 
 
+# See day-2.md about what to do with this code.
+
 role = sagemaker.get_execution_role()
 
 # Hub Model configuration. https://huggingface.co/models
 hub = {
-  'HF_MODEL_ID':'sentence-transformers/multi-qa-mpnet-base-dot-v1', # model_id from hf.co/models
+  'HF_MODEL_ID':'sentence-transformers/all-mpnet-base-v2', # model_id from hf.co/models
   'HF_TASK':'sentence-similarity' # NLP task you want to use for predictions
 }
 
@@ -13,7 +15,7 @@ hub = {
 huggingface_model = HuggingFaceModel(
    env=hub,
    role=role, # iam role with permissions to create an Endpoint
-   transformers_version="4.17", # transformers version used
+   transformers_version="4.17.0", # transformers version used
    pytorch_version="1.10.2", # pytorch version used
    py_version="py38", # python version of the DLC
 )
